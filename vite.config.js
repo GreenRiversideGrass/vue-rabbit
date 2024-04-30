a5de490b3058aa94432956562ccb6d3afc321e8f
YANG <908342671@qq.com>
2024-04-30 20:48:14 +0800

    elementPuls主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 导入对应包
 
 
 // https://vitejs.dev/config/
@@ -16,8 +17,9 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      // 1.配置elementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({importStyle:"sass"})],
+    })
   ],
   resolve: {
     // 实际的路劲转换 @  -> src
@@ -25,4 +27,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
