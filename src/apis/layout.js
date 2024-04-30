d7d7d0308b1e28010ac49c70de56c5987e6b03aa
YANG <908342671@qq.com>
2024-04-30 23:22:38 +0800

    home-banner轮播图

--- src/apis/layout.js
+++ src/apis/layout.js
@@ -1,3 +1,4 @@
 import http from "@/utils/http"
 
+// 获取-全部分类(包含推荐商品)
 export const getCategoryAPI = () => http.get('/home/category/head')
--- src/views/Layout/Home/index.vue
+++ src/views/Layout/Home/index.vue
@@ -1,3 +1,16 @@
+<script setup>
+import HomeCategory from './componets/HomeCategory.vue'
+import HomeBanner from './componets/HomeBanner.vue'
+import HomeNew from './componets/HomeNew.vue'
+import HomeHot from './componets/HomeHot.vue'
+import HomeProduct from './componets/HomeProduct.vue'
+</script>
 <template>
-    <div>我是home</div>
-</template>
+    <div class="container">
+      <HomeCategory />
+      <HomeBanner />
+    </div>
+    <HomeNew />
+    <HomeHot />
+    <homeProduct />
+  </template>
