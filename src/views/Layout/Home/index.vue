4777231edfecbc6a8c625dbc807415a657e454d2
YANG <908342671@qq.com>
2024-05-01 14:22:10 +0800

    home-面板组件封装

--- src/views/Layout/Home/index.vue
+++ src/views/Layout/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './componets/HomeBanner.vue'
 import HomeNew from './componets/HomeNew.vue'
 import HomeHot from './componets/HomeHot.vue'
 import HomeProduct from './componets/HomeProduct.vue'
+import HomePanel from './componets/HomePanel.vue'
 </script>
 <template>
     <div class="container">
@@ -13,4 +14,15 @@ import HomeProduct from './componets/HomeProduct.vue'
     <HomeNew />
     <HomeHot />
     <homeProduct />
+    <!-- 测试面板组件 -->
+    <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
+      <div>
+        <p>新鲜好物 好多商品</p>
+      </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
+        <div>
+          <p>人气推荐 好多商品</p>
+        </div>
+    </HomePanel>
   </template>
