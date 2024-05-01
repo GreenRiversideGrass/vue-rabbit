58aebda4d7e24ce4c02e6e6fd6626344e7f9d87f
YANG <908342671@qq.com>
2024-05-01 21:57:04 +0800

    二级分类-路由滚动行为优化

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,11 @@ const router = createRouter({
       path: '/login',
       component: Login
     },
-  ]
+  ],
+  // 路由跳转时，页面滚动到顶部
+  scrollBehavior() {
+    return { top: 0 }
+  }
 })
 
 export default router
