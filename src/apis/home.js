8078c96cf39f5922df82f00d1830a8310d76a4eb
YANG <908342671@qq.com>
2024-05-01 14:37:30 +0800

    home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,4 +1,11 @@
 import http from "@/utils/http"
 
-// 获取-轮播图数据
+// 获取-轮播图数据banner
 export const getBannerAPI = () => http.get('/home/banner')
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const getNewGoodsAPI = () => http.get('/home/new')
