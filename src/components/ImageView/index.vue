90ada9651d0ea9a102da184d0bcb6cdb04d91328
YANG <908342671@qq.com>
2024-05-02 17:15:00 +0800

    图片预览组件-props适配以及优化操作

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,20 +1,20 @@
 <script setup>
 import { ref,watch } from 'vue'
 import { useMouseInElement } from '@vueuse/core'
-// 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+
+// props适配图片列表
+defineProps({
+  imageList: {
+    type: Array,
+    default: () => []
+  }
+})
+
 
 // 1.小图切换大图显示
 const cativeIndex = ref(0)
 const enterhandler = (i) => {
   cativeIndex.value = i
-
 }
 
 // 2.获取鼠标在元素内的位置
@@ -71,7 +71,7 @@ watch([elementX, elementY],() => {
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
+        backgroundImage: `url(${imageList[cativeIndex]})`,
         backgroundPositionX: `${positionX}px`,
         backgroundPositionY: `${positionY}px`,
       },
