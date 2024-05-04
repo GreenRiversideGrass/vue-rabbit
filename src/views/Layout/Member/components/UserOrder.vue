5a10bc22ce249979263e8d3a771dbd5388317532
YANG <908342671@qq.com>
2024-05-04 18:09:25 +0800

    订单-使用tab切换获取动态数据

--- src/views/Layout/Member/components/UserOrder.vue
+++ src/views/Layout/Member/components/UserOrder.vue
@@ -28,13 +28,19 @@ onMounted(() => {
   getOrderList()
 })
 
+// tab切换
+const tabChange = (type) => {
+  params.value.orderState = type
+  getOrderList()
+}
+
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
-      <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
+      <el-tab-pane  v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
       <div class="main-container">
         <div class="holder-container" v-if="orderList.length === 0">
